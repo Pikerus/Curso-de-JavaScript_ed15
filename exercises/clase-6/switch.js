fc169736f85abc3687e731ab5aa86f393f40db5f
PIKERUS\pablo <pablogarpi@gmail.com>
2020-10-16 21:43:07 +0200

    switch ejemplo

---
+++ exercises/clase-6/switch.js
@@ -0,0 +1,21 @@
+/*
+switch(expresion){
+    estamentos
+}
+*/
+
+switch (n) {
+  case 1:
+    //pasan cosas
+    break;
+  case 2: {
+    //pasan más cosas
+    break;
+  }
+  case 3:
+    //pues otras cosas pasan
+    break;
+  default:
+    //si todo lo anterior falla, pasan cosas aquí
+    break;
+}
