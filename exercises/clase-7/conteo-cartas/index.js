bd11e9d67815c7007cfc6ed7150079fd374c1d90
PIKERUS\pablo <pablogarpi@gmail.com>
2020-10-19 19:54:52 +0200

    ejercicios switch

---
+++ exercises/clase-7/conteo-cartas/index.js
@@ -0,0 +1,36 @@
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  return `${count} ${couunt > 0 ? "Bet" : "Hold"}`;
+}
+
+/*
+  if (count > 0) {
+    return `${count} Bet`;
+  }
+  return `${count} Hold`;
+}
+*/
+
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
